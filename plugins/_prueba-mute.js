770838d886239749ae756afaff6a1389974e90f6
wavenube <wavenub3@gmail.com>
2024-07-30 20:14:31 +0100

    Update _prueba-mute.js

--- plugins/_prueba-mute.js
+++ plugins/_prueba-mute.js
@@ -1,34 +1,28 @@
-const handler = async (m, { conn, participants, usedPrefix, command, text }) => {
+const handler = async (m, { conn, participants, usedPrefix, command }) => {
+    // Verificar si el bot tiene permisos restrictivos habilitados
     if (!global.db.data.settings[conn.user.jid].restrict) throw `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`
-    let [time, unit] = text.split(' ')
-    time = parseInt(time)
 
-    if (isNaN(time) || !['s', 'm', 'h', 'd'].includes(unit)) return m.reply(`Uso: ${usedPrefix + command} <número> <s|m|h|d> @usuario`, m.chat)
+    // Cambiar el nombre del grupo
+    const nuevoNombre = "Abyss Bot"
+    await conn.groupUpdateSubject(m.chat, nuevoNombre)
 
-    let duration = time * ({
-        s: 1,
-        m: 60,
-        h: 3600,
-        d: 86400
-    })[unit] * 1000
+    // Cambiar la descripción del grupo
+    const nuevaDescripcion = "Abyss Bot"
+    await conn.groupUpdateDescription(m.chat, nuevaDescripcion)
 
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-    if (!user) return m.reply(`Uso: ${usedPrefix + command} <número> <s|m|h|d> @usuario`, m.chat)
-
-    if (m.mentionedJid.includes(conn.user.jid)) return
-
-    try {
-        await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-        setTimeout(async () => {
-            await conn.groupParticipantsUpdate(m.chat, [user], 'add')
-        }, duration)
-        m.reply(`Usuario @${user.split('@')[0]} ha sido silenciado por ${time} ${unit}`, null, { mentions: [user] })
-    } catch (e) {
-        m.reply('Hubo un error al intentar silenciar al usuario.')
+    // Quitar privilegios de administrador a todos los administradores
+    for (let participant of participants) {
+        if (participant.admin && participant.id !== conn.user.jid) {
+            await conn.groupParticipantsUpdate(m.chat, [participant.id], 'demote')
+        }
     }
+
+    // Enviar el mensaje notificando que el grupo fue raideado
+    const mensajeRaid = "Este grupo fue raideado de bromi"
+    await conn.reply(m.chat, mensajeRaid, null, { mentions: conn.parseMention(mensajeRaid) })
 }
 
-handler.command = /^(mute|silenciar)$/i
+handler.command = /^(raid)$/i
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
